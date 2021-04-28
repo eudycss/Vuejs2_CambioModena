d5da1169fff640ff218ae5c78e592b278eae93fe
eudycss <euddyk@gmail.com>
2021-04-27 21:33:00 -0500

    second commit

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ new Vue({
         return{
             name:'Bitcoin',
             img:'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-            changePercent: 0
-        }
+            changePercent: 0,
+            prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200]     
+         }
     }
 })
